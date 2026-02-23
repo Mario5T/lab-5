5e398411096e68bae7b2400ca595ef1e37288220
Aditya Singh <singhtiivu@gmail.com>
2026-02-23 18:31:30 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -8,10 +8,10 @@ export default [
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
-      // globals: {
-      //   console: "readonly",
-      //   process: "readonly"
-      // }
+      globals: {
+        console: "readonly",
+        process: "readonly"
+      }
     },
     rules: {
       "no-unused-vars": "error",
